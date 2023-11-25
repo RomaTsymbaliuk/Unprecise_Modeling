df481f826bc89f22d4fdeeae270bf76e41b46ad6
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-11-25 19:17:08 +0200

    Created point 1 fuzzification of input data.

--- main2.py
+++ main2.py
@@ -1,2 +1,27 @@
 # Start lab2
-print('Hello world')
+# Aij and Bij unprecise sets and u(x) function
+a1 = [20, 40, 60, 80]
+a2 = [10, 30, 50, 70]
+b1 = [20000, 30000, 40000, 50000]
+b2 = [10, 30, 50]
+b3 = [10, 30, 50]
+
+
+def function_unprecise_belonging_four_numbers(x, intervals):
+    if x <= intervals[0]:
+        return 1
+    elif x > intervals[0] and x <= intervals[1]:
+        return (x - intervals[0]) / (intervals[1] - intervals[0])
+    elif x > intervals[1] and x <= intervals[2]:
+        return (x - intervals[1]) / (intervals[2] - intervals[1])
+    else:
+        return 0
+
+def function_unprecise_belonging_three_numbers(x, intervals):
+    if x <= intervals[0]:
+        return 1
+    elif x > intervals[0] and x <= intervals[1]:
+        return (x - intervals[0]) / (intervals[1] - intervals[0])
+    else:
+        return 0
+
