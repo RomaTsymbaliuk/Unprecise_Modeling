543f071fcc5f29515ffdfee9a9cf6e4b9846291d
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-11-25 20:56:29 +0200

    Doing point 2 data aggregation.

--- main2.py
+++ main2.py
@@ -6,22 +6,42 @@ b1 = [20000, 30000, 40000, 50000]
 b2 = [10, 30, 50]
 b3 = [10, 30, 50]
 
+class inputData:
+    def __init__(self, area_cottage, area_building, money_benefits, money_losses, money_lost_benefits):
+        self.area_cottage = area_cottage
+        self.area_building = area_building
+        self.money_benefits = money_benefits
+        self.money_losses = money_losses
+        self.money_lost_benefits = money_lost_benefits
+        self.fuzzicated_data = []
+        self.rules =
 
-def function_unprecise_belonging_four_numbers(x, intervals):
-    if x <= intervals[0]:
-        return 1
-    elif x > intervals[0] and x <= intervals[1]:
-        return (x - intervals[0]) / (intervals[1] - intervals[0])
-    elif x > intervals[1] and x <= intervals[2]:
-        return (x - intervals[1]) / (intervals[2] - intervals[1])
-    else:
-        return 0
+    def function_unprecise_belonging(self, x, intervals):
+        if len(intervals) == 4:
+            if x <= intervals[0]:
+                self.fuzzicated_data.append([1, 1])
+            elif x > intervals[0] and x <= intervals[1]:
+                self.fuzzicated_data.append([2, (x - intervals[0]) / (intervals[1] - intervals[0])])
+            elif x > intervals[1] and x <= intervals[2]:
+                self.fuzzicated_data.append([3, (x - intervals[1]) / (intervals[2] - intervals[1])])
+            else:
+                self.fuzzicated_data.append([4, 0])
+        elif len(intervals) == 3:
+            if x <= intervals[0]:
+                self.fuzzicated_data.append([1, 1])
+            elif x > intervals[0] and x <= intervals[1]:
+                self.fuzzicated_data.append([2, (x - intervals[0]) / (intervals[1] - intervals[0])])
+            else:
+                self.fuzzicated_data.append([3, 0])
 
-def function_unprecise_belonging_three_numbers(x, intervals):
-    if x <= intervals[0]:
-        return 1
-    elif x > intervals[0] and x <= intervals[1]:
-        return (x - intervals[0]) / (intervals[1] - intervals[0])
-    else:
-        return 0
+    def fuzzification_data(self):
+        for interval, x in zip([a1, a2, b1, b2, b3], [self.area_cottage, self.area_building, self.money_benefits, self.money_losses, self.money_lost_benefits]):
+            self.function_unprecise_belonging(x, interval)
 
+    def agregation_data(self):
+        pass
+
+
+first_vector = inputData(35, 20, 24000, 5, 12)
+first_vector.fuzzification_data()
+print(first_vector.fuzzicated_data)
