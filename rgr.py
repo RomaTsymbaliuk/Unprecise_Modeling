4426a5ae6cdbe7ad7d5737bd631e75a1a7f5a54d
Roma-x <tsymbaljuk2001@gmail.com>
2024-01-02 19:08:24 +0200

    Good with very low

--- rgr.py
+++ rgr.py
@@ -33,22 +33,22 @@ def plot_graphic():
 def fuzzy_rules_and_system_create():
     FS = FuzzySystem()
 
-    FS.add_linguistic_variable("x", LinguisticVariable([FuzzySet(function=Triangular_MF(a=8.0, b=9.0, c=10.8), term="very_low"),
-                                                        FuzzySet(function=Triangular_MF(a=10.75, b=11.4, c=12.2),term="low"),
-                                                        FuzzySet(function=Triangular_MF(a=11.7, b=12.0, c=12.7), term="average"),
-                                                        FuzzySet(function=Triangular_MF(a=12.5, b=14.3, c=15.8), term="large"),
-                                                        FuzzySet(function=Triangular_MF(a=15.3, b=17.9, c=18), term="very_large")], concept="First arg",
+    FS.add_linguistic_variable("x", LinguisticVariable([FuzzySet(function=Crisp_MF(a=8.0, b=10.8), term="very_low"),
+                                                        FuzzySet(function=Crisp_MF(a=10.75, b=12.2),term="low"),
+                                                        FuzzySet(function=Crisp_MF(a=11.7, b=12.7), term="average"),
+                                                        FuzzySet(function=Crisp_MF(a=12.5, b=15.8), term="large"),
+                                                        FuzzySet(function=Crisp_MF(a=15.3, b=18), term="very_large")], concept="First arg",
                                                             universe_of_discourse=[8, 18]))
 
-    FS.add_linguistic_variable("y", LinguisticVariable([FuzzySet(function=Triangular_MF(a=3.0, b=4.0, c=4.5), term="very_low"),
-                                                        FuzzySet(function=Triangular_MF(a=3.5, b=5.0, c=5.4),term="low"),
+    FS.add_linguistic_variable("y", LinguisticVariable([FuzzySet(function=Gaussian_MF(mu=3.105, sigma=0.2), term="very_low"),
+                                                        FuzzySet(function=Triangular_MF(a=3.2, b=5.0, c=5.4),term="low"),
                                                         FuzzySet(function=Triangular_MF(a=4.2, b=5.0, c=5.4), term="average"),
                                                         FuzzySet(function=Triangular_MF(a=5.3, b=6.2, c=6.7), term="large"),
                                                         FuzzySet(function=Triangular_MF(a=6.6, b=7.6, c=8), term="very_large")], concept="Second arg",
                                                         universe_of_discourse=[3, 8]))
 
-    FS.add_linguistic_variable("z", LinguisticVariable([FuzzySet(function=Triangular_MF(a=0.13823, b=0.5, c=0.7), term="very_low"),
-                                                      FuzzySet(function=Triangular_MF(a=0.6, b=1.0, c=1.2), term="low"),
+    FS.add_linguistic_variable("z", LinguisticVariable([FuzzySet(function=Triangular_MF(a=0.13823, b=0.15832, c=0.17854), term="very_low"),
+                                                      FuzzySet(function=Triangular_MF(a=0.17414, b=0.23421, c=0.65423), term="low"),
                                                       FuzzySet(function=Triangular_MF(a=1.1, b=1.2, c=1.6), term="average"),
                                                       FuzzySet(function=Triangular_MF(a=1.5, b=2.0, c=2.2),term="large"),
                                                       FuzzySet(function=Triangular_MF(a=2.0, b=2.8, c=3.0),term="very_large")], concept = "Third arg",
@@ -61,24 +61,24 @@ def fuzzy_rules_and_system_create():
         "IF (x IS very_low) AND (y IS large) THEN (z IS average)",
         "IF (x IS very_low) AND (y IS very_large) THEN (z IS large)",
         "IF (x IS low) AND (y IS very_low) THEN (z IS very_low)",
-        "IF (x IS low) AND (y IS low) THEN (z IS low)",
+        "IF (x IS low) AND (y IS low) THEN (z IS very_low)",
         "IF (x IS low) AND (y IS average) THEN (z IS low)",
-        "IF (x IS low) AND (y IS large) THEN (z IS low)",
-        "IF (x IS low) AND (y IS very_large) THEN (z IS medium)",
-        "IF (x IS average) AND (y IS very_low) THEN (z IS very_low)",
-        "IF (x IS average) AND (y IS low) THEN (z IS very_low)",
-        "IF (x IS average) AND (y IS average) THEN (z IS low)",
-        "IF (x IS average) AND (y IS large) THEN (z IS low)",
-        "IF (x IS average) AND (y IS very_large) THEN (z IS medium)",
-        "IF (x IS large) AND (y IS very_low) THEN (z IS very_low)",
-        "IF (x IS large) AND (y IS low) THEN (z IS very_low)",
-        "IF (x IS large) AND (y IS average) THEN (z IS low)",
-        "IF (x IS large) AND (y IS large) THEN (z IS low)",
-        "IF (x IS large) AND (y IS very_large) THEN (z IS medium)",
-        "IF (x IS very_large) AND (y IS very_low) THEN (z IS very_low)",
-        "IF (x IS very_large) AND (y IS low) THEN (z IS very_low)",
-        "IF (x IS very_large) AND (y IS average) THEN (z IS low)",
-        "IF (x IS very_large) AND (y IS large) THEN (z IS low)",
+        "IF (x IS low) AND (y IS large) THEN (z IS average)",
+        "IF (x IS low) AND (y IS very_large) THEN (z IS large)",
+        "IF (x IS average) AND (y IS very_low) THEN (z IS low)",
+        "IF (x IS average) AND (y IS low) THEN (z IS low)",
+        "IF (x IS average) AND (y IS average) THEN (z IS average)",
+        "IF (x IS average) AND (y IS large) THEN (z IS average)",
+        "IF (x IS average) AND (y IS very_large) THEN (z IS large)",
+        "IF (x IS large) AND (y IS very_low) THEN (z IS low)",
+        "IF (x IS large) AND (y IS low) THEN (z IS average)",
+        "IF (x IS large) AND (y IS average) THEN (z IS large)",
+        "IF (x IS large) AND (y IS large) THEN (z IS very_large)",
+        "IF (x IS large) AND (y IS very_large) THEN (z IS very_large)",
+        "IF (x IS very_large) AND (y IS very_low) THEN (z IS average)",
+        "IF (x IS very_large) AND (y IS low) THEN (z IS average)",
+        "IF (x IS very_large) AND (y IS average) THEN (z IS large)",
+        "IF (x IS very_large) AND (y IS large) THEN (z IS very_large)",
         "IF (x IS very_large) AND (y IS very_large) THEN (z IS very_large)"
     ]
 
@@ -100,14 +100,20 @@ result_fuzzy_var = use_fuzzy_system(FS, 8, 3)
 
 x = 8.1
 y = 3.1
+delta_sum = 0
+steps = 1
 
-while x < 10.9:
+while x < 10.9 and steps <= 50:
     while y < 8:
 #        fuzzy_var = use_fuzzy_system(FS, x, y) * 1000000
-#        delta = fuzzy_var - function(x, y)
         func_value = function(x, y)
-        fuzzy_var = use_fuzzy_system(FS, x, y) * 1000000
+        fuzzy_var = use_fuzzy_system(FS, x, y) * 100000
+        delta = math.fabs(fuzzy_var - function(x, y))
+        print('x : ', x, ' y : ', y, ' fuzzy_var : ', fuzzy_var, ' func_value : ', func_value, ' delta : ', delta)
         delta = math.fabs(fuzzy_var - func_value)
-        print('x : ', x, 'y : ', y, 'fuzzy_var : ', fuzzy_var, ' function : ', func_value, ' delta : ', delta)
+        delta_sum = delta_sum + delta
         y = y + 0.1
-    x = x + 0.5
+    steps = steps + 1
+    x = x + 0.1
+
+print('delta_sum : ', delta_sum / 1000000)
